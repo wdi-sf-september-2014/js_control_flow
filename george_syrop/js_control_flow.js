69b9a4d635fb7a772c0899dbe5bfb7310c3af643
George Syrop <george@syrop.co>
2014-09-29 13:51:49 -0700

    Solved JS HW

--- george_syrop/js_control_flow.js
+++ george_syrop/js_control_flow.js
@@ -1,1 +1,24 @@
-//Your code here
+function powerOf(base,exponent) {
+    if (base == 1) {
+        return 1;
+	}
+	if (exponent === 0) {
+		return 1;
+	}
+	else {
+        var count = 0;
+		var newNum = 1;
+        while (count < exponent) {
+			newNum *= base;
+			count ++;
+		}
+        return newNum;
+	}
+
+}
+
+//tests
+console.log(powerOf(9,0));
+console.log(powerOf(2,3));
+console.log(powerOf(1,25));
+console.log(powerOf(4,4));
