a124445529a84f9e649b4620fe709a60b07d16c5
Myla Baker <myla.baker@gmail.com>
2014-09-30 15:30:36 -0700

    added my homework

--- myla_baker/js_control_flow.js
+++ myla_baker/js_control_flow.js
@@ -1,1 +1,9 @@
-//Your code here
+function calculateExponent(b, e) {
+	var result = 1;
+    for (var i=0; i < e; i++) {
+    result = result * b;
+    };
+    console.log(result);
+};
+
+calculateExponent(2,5);
