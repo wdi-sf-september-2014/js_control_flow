b3a21301a7524c287cf565a314dd1bbaa042a284
rragno <robertragno@gmail.com>
2014-09-30 16:43:49 -0700

    Math simplification

--- robert_ragno/js_control_flow.js
+++ robert_ragno/js_control_flow.js
@@ -4,7 +4,7 @@ var calculateExponent = function(num1,num2) {
     
     //The long way
     var value = num1;
-	for (var i = 1; i <= num2-1; i++) {
+	for (var i = 1; i < num2; i++) {
 		value = value * num1;
 	}
 	console.log(value);
