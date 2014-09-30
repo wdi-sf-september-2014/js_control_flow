cabd303421ad87db3be8fef667b475971d0ff7a0
Sandi <sandima.acro@gmail.com>
2014-09-30 16:08:05 -0700

    Sandi's code

--- sandi_ma/js_control_flow.js
+++ sandi_ma/js_control_flow.js
@@ -1,1 +1,10 @@
+var base = prompt("Please enter a base number");
+var expon = prompt("Please enter the exponent");
+
+var calculateExponent = function(base, expon) {
+    console.log("Your answer is " + Math.pow(base, expon));
+    
+}
+calculateExponent(base, expon);
+
 //Your code here
