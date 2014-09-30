89777ac5b6380ff3b016f032a7fd49fe8a47c19f
rragno <robertragno@gmail.com>
2014-09-30 16:25:01 -0700

    Simplified solution slightly.

--- robert_ragno/js_control_flow.js
+++ robert_ragno/js_control_flow.js
@@ -4,11 +4,8 @@ var calculateExponent = function(num1,num2) {
     
     //The long way
     var value = num1;
-    var total;
 	for (var i = 1; i <= num2-1; i++) {
-  
-		total = value * num1;
-        value = total;
+		value = value * num1;
 	}
 	console.log(value);
 }
