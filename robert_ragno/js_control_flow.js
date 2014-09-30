5fc4749e49f945838ace22be0e48e0c2eb6b4ec1
rragno <robertragno@gmail.com>
2014-09-30 16:20:06 -0700

    Add solution for tonight's homework.

--- robert_ragno/js_control_flow.js
+++ robert_ragno/js_control_flow.js
@@ -1,1 +1,16 @@
-//Your code here
+var calculateExponent = function(num1,num2) {
+	//The easy way
+	//console.log(Math.pow(num1, num2));
+    
+    //The long way
+    var value = num1;
+    var total;
+	for (var i = 1; i <= num2-1; i++) {
+  
+		total = value * num1;
+        value = total;
+	}
+	console.log(value);
+}
+
+calculateExponent(2,10);
