53866bfae8a370243896242e951f15df3aebe911
Nicole Borgaard <nicoleborgaard@gmail.com>
2014-10-01 07:48:01 -0700

    homework

--- nicole_borgaard/js_control_flow.js
+++ nicole_borgaard/js_control_flow.js
@@ -1,1 +1,12 @@
 //Your code here
+
+// JS Control Flow 
+
+function calcExp(base, exponent) {
+ var product = 1;
+ for (var i = 1; i <= exponent; i++) {
+  product = product * base;
+ }
+ return product
+};
+
