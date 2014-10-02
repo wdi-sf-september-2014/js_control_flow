dc944d3f08e9a9c9220075a290165ac357149ac1
Myla Baker <myla.baker@gmail.com>
2014-10-01 17:22:03 -0700

    Altered based on notes

--- myla_baker/js_control_flow.js
+++ myla_baker/js_control_flow.js
@@ -1,9 +1,13 @@
 function calculateExponent(b, e) {
 	var result = 1;
+	if (e < 0) {
+    result= (1.0/(result * b));
+  } else {
     for (var i=0; i < e; i++) {
-    result = result * b;
-    };
-    console.log(result);
+      result = result * b;
+    }
+  };
+  return result;
 };
 
-calculateExponent(2,5);
+calculateExponent(2,-1); 
